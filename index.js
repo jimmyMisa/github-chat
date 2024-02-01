559ad526590f160cd04788815d13521bc284629b
jimmyMisa <jimmysamuel_andriamparany@yahoo.com>
2024-02-01 15:22:52 +0300

    review

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run() {
                         url: 'https://api.openai.com/v1/chat/completions',
                         headers: {
                             'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${apiKey}ss`
+                            'Authorization': `Bearer ${apiKey}`
                         },
                         data: data
                     };
