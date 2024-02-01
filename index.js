e5432c034b2190b5c9253aaa2b4fbcf84972d3cf
jimmyMisa <jimmysamuel_andriamparany@yahoo.com>
2024-02-01 15:42:02 +0300

    review

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 async function run() {
     try {
         const apiKey = process.env.OPENAI_API_KEY;
+        const gatoken = process.env.GA_TOKEN;
         const branch = core.getInput('branch');
         const repoOwner = process.env.GITHUB_REPOSITORY.split('/')[0];
         const repoName = process.env.GITHUB_REPOSITORY.split('/')[1];
@@ -42,8 +43,7 @@ async function run() {
                         url: 'https://api.openai.com/v1/chat/completions',
                         headers: {
                             'Content-Type': 'application/json',
-                            //'Authorization': `Bearer ${apiKey}`
-                            'Authorization': `Bearer ${"sk-mFS7Zc9SGFb9PwKQdmZrT3B"+""+""+"lbkFJqIefVdi9ylr9lDtPNDIt"}`
+                            'Authorization': `Bearer ${apiKey}`
                         },
                         data: data
                     };
@@ -82,7 +82,8 @@ async function run() {
                         }, {
                             headers: {
                                 'Content-Type': 'application/json',
-                                'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
+                                //'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
+                                'Authorization': `Bearer ${gatoken}`,
                             },
                         }
                     );
